5c1f06ffcf2df8bc871e7028efcedec5c8a18778
ccuellar2007 <ccuellar2007@berkeley.edu>
2018-07-20 18:59:53 -0400

    project environment set up, no index work yet, preliminary user model flushed out and working on the home model

--- life/models.py
+++ life/models.py
@@ -3,3 +3,34 @@ from django.db import models
 class Group(models.Model):
 	established = models.DateTimeField(auto_now_add=True)
 	name = models.CharField(max_length=50)
+  
+class User(models.Model):
+    username = models.CharField(max_length=30, primary_key=True)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    photo = models.CharField(max_length=500)
+    hometown = models.CharField(max_length=30)
+    # many to many object of revies
+    # search
+    
+    
+class Home(models.Model):
+  address = models.CharField(max_length=50, primary_key=True)
+  price = models.
+
+    
+    
+# Username
+# String
+# Password
+# String
+# Name
+# String 
+# Photo
+# String
+# Hometown
+# String 
+# Reviews
+# Array of strings to User Review Models
+# Search
+# String 
