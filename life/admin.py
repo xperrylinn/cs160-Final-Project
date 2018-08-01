c4d467fbf0b1e281b4fc5683a233efa847e14208
xperrylinn <xperrylinn@berkeley.edu>
2018-08-01 12:06:17 -0400

    removing life/* and p5/* files

--- life/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from life.models import *
-
-# Register your models here.
-admin.site.register(Group)
-admin.site.register(User)
-admin.site.register(Home)
-admin.site.register(Review)
